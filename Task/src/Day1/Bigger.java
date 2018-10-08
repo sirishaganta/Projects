ff49afe0e7223a1e01a0b6a3b777bcaebc282d47
sirishaganta <43954160+sirishaganta@users.noreply.github.com>
2018-10-08 19:44:22 +0530

    DisplayBiggestElement
    completed

---
+++ Task/src/Day1/Bigger.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class Bigger {
+	public static void main(String[] args) {
+		int a=10;
+		int b=13;
+		if(a-b>=2)
+		{
+			System.out.println(a);
+		}
+		else if(b-a>=2)
+		{
+			System.out.println(b);
+		}
+		else
+		{
+			System.out.println("INCONCLUSIVE");
+		}
+	}
+
+}
